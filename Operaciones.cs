8cbf69da77ccb5d2b8141dfcce34469e6d0107c0
Javis <javieralvarez172005@gmail.com>
2024-12-12 19:39:36 -0600

    metodo

---
+++ Operaciones.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_unidad_3
+{
+    internal class Operaciones
+    {
+        public int NumeroA { get; set; }
+        public int NumeroB { get; set; }
+
+        public int Sumar()
+        {
+            return NumeroA + NumeroB;
+        }
+
+        public int Restar()
+        {
+            return NumeroA - NumeroB;
+        }
+
+        public int Multiplicar()
+        {
+            return NumeroA * NumeroB;
+        }
+
+        public int Dividir()
+        {
+            if (NumeroB == 0)
+            {
+                Console.WriteLine("Error: No se puede dividir entre cero.");
+                return 0;
+            }
+            return NumeroA / NumeroB;
+        }
+    }
+}
